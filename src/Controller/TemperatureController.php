2d4113093778a9df9e45199b56c91560fa1a18d6
Pavel-Kliukin <9213087@mail.ru>
2023-04-28 10:47:49 +0300

    Task 2 done

--- src/Controller/TemperatureController.php
+++ src/Controller/TemperatureController.php
@@ -3,13 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TemperatureController {
 
-  #[Route('/')]
-  public function hellosymfony() {
-    return new Response("Hello Symfony!");
+  #[Route('/Fahr')]
+  public function fahren(Request $request) {
+
+    $temp = $request->query->get('temperature');
+    if (!is_numeric($temp)) {
+      // Return an error if the temperature is invalid
+      return new Response("Error: Temperature must be a number", 400);
+      }
+
+    $fahrenheit = ($temp * 9 / 5) + 32;
+    return new Response($fahrenheit);
   }
 
 }
